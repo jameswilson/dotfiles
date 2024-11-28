77898c5810c22050d94fe67a833c567d88a39ace
James Wilson <jrwilson3@gmail.com>
2024-11-28 17:44:40 -0500

    script to install homebrew and preferred formulae

---
+++ bin/brew.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+if ! command -v brew 2>&1 >/dev/null
+then
+  echo "brew command could not be found; installing Homebrew..."
+  echo "/bin/bash -c \"\$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)\""
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Install iTerm2
+if [ ! -d /Applications/iTerm.app ]
+then
+  brew install iterm2
+fi
+
+# Install GNU core utilities (those that come with macOS are outdated).
+# This requires `$(brew --prefix coreutils)/libexec/gnubin` in $PATH.
+# @see ~/.environment
+brew install coreutils
+# Install some other useful utilities like `parallel`, `ts`.
+brew install moreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+# This requires `$(brew --prefix findutils)/libexec/gnubin` in $PATH.
+# @see ~/.environment
+brew install findutils
+# Install GNU `sed`, overwriting the built-in `sed`.
+brew install gnu-sed
+
+# Install latest versions of some macOS tools.
+brew install grep
+brew install screen
+brew install vim
+brew install git
+
+# Install other useful binaries.
+brew install imagemagick
+brew install wget
+brew install tree
+
+# Install macOs Quick Look plugins.
+# Try also `brew search --desc "Quick Look plugin"`
+brew install qlcolorcode
+brew install qlstephen
+brew install qlmarkdown
+brew install quicklook-json
+brew install betterzip
+brew install qlprettypatch
+brew install webpquicklook
+brew install suspicious-package
+brew install syntax-highlight
+
+# Install developer tools.
+brew install ddev/ddev/ddev
+
+# Remove outdated versions from the cellar.
+brew cleanup
